2d2d44e47bef7ae1a4341ef0d816e95055c50166
sneelyg <santiagoneely@gmail.com>
2023-05-19 09:28:08 -0400

    second class

--- app.js
+++ app.js
@@ -1,23 +1,81 @@
-import { Persona } from "./Persona.js";
+// import { Persona } from "./Persona.js";
 
-const saludar = () => {
-  console.log("Hola!");
-};
+//createApp
+//Destructuring es como poner const createApp = Vue.createApp, crea la variable y la va a bsucar al bojeto
+const { createApp } = Vue;
 
-saludar();
-const persona_1 = new Persona('Robert','Galmez', 22, "Chile")
-console.log(persona_1)
+createApp({
+  data() {
+    return {
+      contador: 0,
+      titulo: "Esta es la página de",
+      bajada: " está usando VueJS, un framework de javascript ",
+      salida: "Ya shau!!",
+      nombre: "Santiago",
+      nombre_input: "",
+      edad: 31,
+      ciudad: "Pichilemu",
+      a: 20,
+      b: 15,
+      numero: 13,
+      laHora: "",
+      mensaje: "El Heladero aumentó los precios",
+      div: `
+      <h3>Hola desde v-html</h3>
+      <p>El Heladoero bajó sus precios</p>
+      `,
+      url: "https://chileestuyo.cl/wp-content/uploads/2019/09/wave-1246560_1920.jpg",
+      parrafo: "",
 
-const name = "Santiago";
+      classBtn: {
+        "background-color": "red",
+        cursor: "pointer",
+      },
+      mostrar: true,
+      nombres: ["Brian", "Pedro"],
+    };
+  },
+  created() {
+    // Created es para lo que se ejecuta al cargar la página.
+    this.calculoFuncion();
+  },
+  methods: {
+    aumentar(){
+      this.contador += 1;
+    },
+    disminuir(){
+      this.contador -= 1;
+    },
+    presentacion() {
+      return this.nombre + " tiene " + this.edad + " años!";
+    },
+    calculoFuncion() {
+      return this.a * this.b;
+    },
+    sumas() {
+      return this.numero + 100;
+    },
+    multiplicado() {
+      this.numero *= 10;
+    },
 
-const phrase = `hola ${persona_1.nombre}`
-console.log(phrase); 
-
-const promesa = new Promise(function (resolve, reject){
-    setTimeout(function() { resolve("Promess cumplida"); }, 3000);
-
-});
-
-promesa.then(function(result){
-    console.log(`${result} para ${persona_1.nombre}`)
-});
+    explicacion() {
+      return (
+        "Esta funcion multiplica " +
+        this.a +
+        " por " +
+        this.b +
+        ", y el resultado es : " +
+        this.calculoFuncion()
+      );
+    },
+    saludar() {
+      console.log("Hola " + this.nombre_input);
+    },
+  },
+  computed: {
+    sumaComputed() {
+      return this.numero * this.a;
+    },
+  },
+}).mount("#app");
