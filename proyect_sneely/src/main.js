89f033854da5a52674ea52602680c12d151df34e
sneelyg <santiagoneely@gmail.com>
2023-06-27 20:06:03 -0400

    empezando con Vuex

--- proyect_sneely/src/main.js
+++ proyect_sneely/src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
 import 'bootstrap' 
+import Vuex from 'vuex'
+import store from './store'
 
 
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
-
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
---
+++ proyect_sneely/src/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    // Define tus estados iniciales aquí
+  },
+  mutations: {
+    // Define tus mutaciones aquí
+  },
+  actions: {
+    // Define tus acciones aquí
+  },
+  getters: {
+    // Define tus getters aquí
+  }
+})
+
+export default store
